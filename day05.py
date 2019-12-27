7c0f6a14e5c3a8bfd841fe0cc022449c83bb7489
Adam Romig <adam.romig@gmail.com>
2019-12-27 00:41:53 -0500

    day 5 done - rewritten approach

--- day05.py
+++ day05.py
@@ -1,90 +1,71 @@
 import file
 instructions = file.read('inputs/day05.txt').split(',')
 
-HALT = 99
-ADD = 1
-MULT = 2
-INPUT = 3
-OUTPUT = 4
 
-POSITION_MODE = 0
-IMMEDIATE_MODE = 1
+def getParams(intcode, pointer, modes):
+    mode_a = modes.pop() if len(modes) > 0 else 0
+    mode_b = modes.pop() if len(modes) > 0 else 0
+
+    a = intcode[pointer + 1] if mode_a == 1 else intcode[intcode[pointer + 1]]
+    b = intcode[pointer + 2] if mode_b == 1 else intcode[intcode[pointer + 2]]
+    c = intcode[pointer + 3] if pointer + 3 < len(intcode) else None
+
+    return (a, b, c)
 
 
-def runInstructions(intcode, input):
+def runTest(intcode, input):
     pointer = 0
-    while intcode[pointer] != HALT or pointer > len(intcode):
-        if len(str(intcode[pointer])) == 1:
-            if intcode[pointer] == INPUT:
-                inputAddress = intcode[intcode.index(INPUT) + 1]
-                intcode[inputAddress] = input
-                pointer += 2
-            elif intcode[pointer] == OUTPUT:
-                outputAddress = intcode[pointer + 1]
-                print(intcode[outputAddress])
-                pointer += 2
-            else:
-                pos_1 = intcode[pointer + 1]
-                val_1 = intcode[pos_1]
-                pos_2 = intcode[pointer + 2]
-                val_2 = intcode[pos_2]
-                store = intcode[pointer + 3]
+    output = []
 
-                if intcode[pointer] == ADD:
-                    intcode[store] = val_1 + val_2
-                if intcode[pointer] == MULT:
-                    intcode[store] = val_1 * val_2
-                pointer += 4
+    while intcode[pointer] != 99:
+        instruction = str(intcode[pointer])
+        opcode, modes = int(
+            instruction[-2:]), list(map(int, instruction[:-2]))
 
+        if opcode == 1:
+            a, b, c = getParams(intcode, pointer, modes)
+            intcode[c] = a + b
+        elif opcode == 2:
+            a, b, c = getParams(intcode, pointer, modes)
+            intcode[c] = a * b
+        elif opcode == 3:
+            inputPosition = intcode[pointer + 1]
+            intcode[inputPosition] = input
+        elif opcode == 4:
+            outputPosition = intcode[pointer + 1]
+            output.append(intcode[outputPosition])
+        elif opcode == 5:
+            a, b, _ = getParams(intcode, pointer, modes)
+            if a != 0:
+                pointer = b
+                continue
+        elif opcode == 6:
+            a, b, _ = getParams(intcode, pointer, modes)
+            if a == 0:
+                pointer = b
+                continue
+        elif opcode == 7:
+            a, b, c = getParams(intcode, pointer, modes)
+            intcode[c] = 1 if a < b else 0
+        elif opcode == 8:
+            a, b, c = getParams(intcode, pointer, modes)
+            intcode[c] = 1 if a == b else 0
         else:
-            instruction = str(intcode[pointer])
-            instruction = instruction.zfill(5)
-            opcode = int(instruction[-2:])
-            mode_1 = int(instruction[-3:-2])
-            mode_2 = int(instruction[-4:-3])
-            mode_3 = int(instruction[-5:-4])
+            print('Unknown opcode: {}'.format(opcode))
+            return [999]
 
-            if opcode == OUTPUT:
-                if mode_1 == 0:
-                    print(intcode[pointer + 1])
-                if mode_2 == 1:
-                    outputAddress = intcode[pointer + 1]
-                    print(intcode[outputAddress])
-                skip = 2
-            else:
-                if mode_1 == 0:
-                    pos_1 = intcode[pointer + 1]
-                    val_1 = intcode[pos_1]
-                if mode_1 == 1:
-                    val_1 = intcode[pointer + 1]
-                if mode_2 == 0:
-                    pos_2 = intcode[pointer + 2]
-                    val_2 = intcode[pos_2]
-                if mode_2 == 1:
-                    val_2 = intcode[pointer + 2]
-                if mode_3 == 0:
-                    store = intcode[pointer + 3]
+        if opcode == 3 or opcode == 4:
+            pointer += 2
+        elif opcode == 5 or opcode == 6:
+            pointer += 3
+        else:
+            pointer += 4
 
-                if opcode == ADD:
-                    intcode[store] = val_1 + val_2
-                if opcode == MULT:
-                    intcode[store] = val_1 * val_2
-
-                skip = 4
-
-            pointer += skip
-
-    return intcode
+    return output
 
 
-def getInputAddress(intcode):
-    return intcode[intcode.index(INPUT) + 1]
+intcode = [int(num) for num in instructions]
+print('Part One: {}'.format(runTest(intcode, 1)[-1]))
 
-
-def partOne():
-    print("Part One:")
-    intcode = [int(num) for num in instructions]
-    return runInstructions(intcode, 1)
-
-
-partOne()
+intcode = [int(num) for num in instructions]
+print('Part Two: {}'.format(runTest(intcode, 5)[-1]))
